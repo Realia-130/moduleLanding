806414c5f59acea282febdf0013fe4ad91dd6fe6
Michael A Stringer <mastringer87@gmail.com>
2020-09-26 00:02:39 -0700

    cleaned up comments and cleaned up database files

--- database/index.js
+++ database/index.js
@@ -10,25 +10,9 @@ mongoose.connect('mongodb://localhost/listings', {
 });
 
 const seedListings = (callback, newListing) => {
-  const listing = new model.Listing({
-    // eslint-disable-next-line no-underscore-dangle
-    listing_id: newListing.listing_id,
-    listing_photos: [],
-    listing_type: newListing.listing_type,
-    listing_is_saved: false,
-    listing_is_new: newListing.listing_is_new,
-    listing_data: {
-      address: newListing.listing_data.address,
-      city: newListing.listing_data.city,
-      state: newListing.listing_data.state,
-      neighborhood: newListing.listing_data.neighborhood,
-      bed_count: newListing.listing_data.bed_count,
-      bath_count: newListing.listing_data.bath_count,
-      sqft: newListing.listing_data.sqft,
-      starting_price: newListing.listing_data.starting_price,
-      current_price: newListing.listing_data.current_price,
-    },
-  });
+  let listing = new model.Listing();
+  listing = Object.assign(listing, newListing);
+
   listing.save()
     .then(() => callback(null, 'saved'))
     .catch((error) => callback(error));
@@ -41,25 +25,25 @@ const getAllListings = (callback) => {
 };
 
 const getListing = (callback, id) => {
-  model.Listing.find(id)
+  model.Listing.find({ listing_id: id })
     .then((results) => callback(null, results))
     .catch((error) => callback(error));
 };
 
 const scheduleTour = (callback, tourInfo) => {
-  //  temp until schema setup
   callback(tourInfo);
 };
 
 const updateSave = (callback, listingId, newValue) => {
-  // eslint-disable-next-line max-len
-  model.Listing.updateOne({ listing_id: listingId }, { listing_is_saved: newValue }, (error, result) => {
+  const errorFunction = (error, result) => {
     if (error) {
       callback(error);
     } else {
       callback(null, result);
     }
-  });
+  };
+
+  model.Listing.updateOne({ listing_id: listingId }, { listing_is_saved: newValue }, errorFunction);
 };
 
 const removeListing = (callback, listingId) => {
